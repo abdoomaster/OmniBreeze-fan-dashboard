2abf51c08a0a5854b0b6d845929b77617890434d
abdoomaster <root@proxmox.local>
2026-06-13 17:00:56 -0400

    Add OmniBreeze integration constants

---
+++ custom_components/omnibreeze/const.py
@@ -0,0 +1,13 @@
+DOMAIN = "omnibreeze"
+
+CONF_EMAIL = "email"
+CONF_PASSWORD = "password"
+CONF_USER_DOMAIN = "user_domain"
+CONF_USER_DOMAIN_SECRET = "user_domain_secret"
+
+DEFAULT_USER_DOMAIN = "U.SP.8589934603"
+
+API_BASE = "https://iot-api.netprisma.us"
+MQTT_URL = "wss://iot-south.netprisma.us:8443/ws/v2"
+
+PLATFORMS = ["fan", "sensor", "switch"]
